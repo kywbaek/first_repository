37e7727b17626424a3a73443ff5a71171840cff9
Young Wook Baek <kywbaek@gmail.com>
2021-06-09 13:19:33 -0400

    Remove two lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
   public static void main(String []) {
     System.out.println("Hello World");
-    System.out.println("Line #2");
-    System.out.println("Line #3");
   }
 }
